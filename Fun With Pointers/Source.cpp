0725bd0d0f449822d3890b3b72c09aa489920ed6
Zack Driscoll <zacdrisc@uat.edu>
2020-03-31 14:25:12 -0700

    Add project files.

---
+++ Fun With Pointers/Source.cpp
@@ -0,0 +1,49 @@
+//Learning how to use Pointers
+//Author: Zack Driscoll
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Title of the application
+	cout << "\n\n\t\t *** Pointers are Easy *** \n";
+
+	//If you have a ref, you have to have something to refer to
+	int age = 21;
+	
+
+	//************************ Create the ref to age ***************************
+
+	//A ref will never be different from what it is referencing
+	int &r_age = age;
+
+	//Print values
+	cout << "\nMy age from the variable age says: " << age << endl;
+	cout << "\nMy age from the reference r_age says: " << r_age << endl;
+
+	//Change the value of the ref as an example
+	cout << "\nNow I'm changing the ref r_age value\n";
+	r_age = 25;
+
+	//Print values
+	cout << "\nMy age from the variable age says: " << age << endl;
+	cout << "\nMy age from the reference r_age says: " << r_age << endl;
+
+
+	//************************ Pointer Land Below *****************************
+
+	//Pointers can only hold one thing: Memory Address
+	//Here &age is calling on the memory address for the variable age
+	int *p_age = &age;
+
+	//Print values
+	cout << "\nMy age from the variable age says: " << age << endl;
+	cout << "\nMy age from the pointer p_age says: " << p_age << endl;
+	cout << "\nMy age from the from dereferencing the pointer p_age says: " << *p_age << endl;
+
+
+
+	return 0;
+}
